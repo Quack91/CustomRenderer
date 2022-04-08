c239ad5e590950185886bc23fdba2702fa0fd41f
Quack <dmiller91ca@gmail.com>
2022-04-08 20:10:00 +0200

    implemented dda line drawing algorithm that uses rounding and how every step affects the line

--- Renderer.cpp
+++ Renderer.cpp
@@ -12,20 +12,22 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 const TGAColor white = TGAColor(255, 255, 255, 255);
 
-void draw_line(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color);
+void draw_line_naive(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color);
+void draw_line_dda(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color);
+
 
 int main()
 {
 	TGAImage image(width, height, TGAImage::RGB);
 
-	draw_line(40, 10, 10, 20, image, white);
+	draw_line_dda(40, 10, 10, 20, image, white);
 	// Image is flipped vertically, to show it from bottom-left corner.
 	image.flip_vertically();
 
 	image.write_tga_file("output.tga");
 }
 
-void draw_line(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color)
+void draw_line_naive(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color)
 {
 	if (x2 < x1)
 	{
@@ -58,6 +60,36 @@ void draw_line(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& 
 		{
 			image.set(x, y, color);
 		}
+	}
+}
+
+void draw_line_dda(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color)
+{
+	float x, y, dx, dy, step;
+
+	x = x1;
+	y = y1;
+
+	dx = x2 - x1;
+	dy = y2 - y1;
+
+	if (std::abs(dx) >= std::abs(dy))
+		step = std::abs(dx);
+	else
+		step = std::abs(dy);
+
+	if (!step) 
+		return;
+
+	dx = dx / step;
+	dy = dy / step;
+
+	for (int i = 1; i <= step; i++)
+	{
+		image.set(x, y, color);
+
+		x += dx;
+		y += dy;
 	}
 }
 
