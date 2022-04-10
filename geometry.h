f75218cb0c12f8536cbb8e40eb51a3a8a4732fd6
Quack <dmiller91ca@gmail.com>
2022-04-10 23:33:45 +0200

    Added vector3 implementation

--- geometry.h
+++ geometry.h
@@ -17,9 +17,9 @@ struct Vec2
 
 	inline Vec2<T> operator +(const Vec2<T>& V) { return Vec2<T>(x + V.x, y + V.y); }
 	inline Vec2<T> operator -(const Vec2<T>& V) { return Vec2<T>(x - V.x, y - V.y); }
-	inline Vec2<T> operator *(T value) { return Vec2<T>(x * value, y * value); }
-	inline Vec2<T> operator /(T value) { return Vec2<T>(x / value, y / value); }
-	inline T operator [](int index) { return raw[index]; }
+	inline Vec2<T> operator *(T Value) { return Vec2<T>(x * Value, y * Value); }
+	inline Vec2<T> operator /(T Value) { return Vec2<T>(x / Value, y / Value); }
+	inline T operator [](int Index) { return raw[Index]; }
 
 	inline Vec2<T>& operator +=(const Vec2<T>& V) {
 		x += V.x;
@@ -33,15 +33,15 @@ struct Vec2
 		return *this;
 	}
 
-	inline Vec2<T>& operator *=(const T value) {
-		x *= value;
-		y *= value;
+	inline Vec2<T>& operator *=(const T Value) {
+		x *= Value;
+		y *= Value;
 		return *this;
 	}
 
-	inline Vec2<T>& operator /=(const T value) {
-		x /= value;
-		y /= value;
+	inline Vec2<T>& operator /=(const T Value) {
+		x /= Value;
+		y /= Value;
 		return *this;
 	}
 
@@ -55,7 +55,64 @@ struct Vec2
 		return *this / Length();
 	}
 };
+template<class T>
+struct Vec3
+{
+	union {
+		struct { T x, y, z; };
+		struct { T ivert, iuv, inorm; };
+		T raw[3];
+	};
 
+	Vec3(T X = 0, T Y = 0, T Z = 0) : x(X), y(Y), z(Z) { }
+
+	inline Vec3<T> operator +(const Vec3<T>& V) { return Vec3<T>(x + V.x, y + V.y, z + V.z); }
+	inline Vec3<T> operator -(const Vec3<T>& V) { return Vec3<T>(x - V.x, y - V.y, z - V.z); }
+	inline Vec3<T> operator *(T Value) { return Vec3<T>(x * Value, y * Value, z * Value); }
+	inline Vec3<T> operator /(T Value) { return Vec3<T>(x / Value, y / Value, z/Value); }
+	inline T operator [](int index) { return raw[index]; }
+
+	inline Vec3<T>& operator +=(const Vec3<T>& V) {
+		x += V.x;
+		y += V.y;
+		z += V.z;
+		return *this;
+	}
+
+	inline Vec3<T>& operator -=(const Vec3<T>& V) {
+		x -= V.x;
+		y -= V.y;
+		z -= V.z;
+		return *this;
+	}
+
+	inline Vec3<T>& operator *=(const T Value) {
+		x *= Value;
+		y *= Value;
+		z *= Value;
+		return *this;
+	}
+
+	inline Vec3<T>& operator /=(const T Value) {
+		x /= Value;
+		y /= Value;
+		z /= Value;
+		return *this;
+	}
+
+	inline static float DotProduct(const Vec3<T>& lhs, const Vec3<T>& rhs) { return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z; }
+
+	inline float Length() {
+		return std::sqrt(x * x + y * y + z*z);
+	}
+
+	inline Vec2<T> GetNormalized() {
+		return *this / Length();
+	}
+};
 
 typedef Vec2<float> Vector2;
 typedef Vec2<int> Vector2Int;
+
+typedef Vec3<float> Vector3;
+typedef Vec3<int> Vector3Int;
